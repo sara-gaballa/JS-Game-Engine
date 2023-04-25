95f48cb19ce70baa8816151e4957802c3bab24f6
NancyHisham <96303049+NancyHisham@users.noreply.github.com>
2023-04-25 04:57:26 +0200

    grid

--- Games/Checkers/Checkers.js
+++ Games/Checkers/Checkers.js
@@ -1,54 +1,56 @@
 import { GameEngine } from "../../Game-Engine/Engine-abstract.js";
+
 export default class Checkers extends GameEngine {
-    constructor() {
-        super([8, 8], ["red", "black"]);
-        this.currentPlayer = 0;
-      }
-    
-      draw_board() {
-        const board = document.getElementById("board");
-        for (let i = 0; i < 8; i++) {
-          const row = document.createElement("tr");
-          for (let j = 0; j < 8; j++) {
-            const cell = document.createElement("td");
-            if ((i + j) % 2 === 0) {
-              cell.className = "black";
-              if (i < 3) {
-                const piece = document.createElement("div");
-                piece.className = "piece red";
-                cell.appendChild(piece);
-                } else if (i > 4) {
-                const piece = document.createElement("div");
-                piece.className = "piece white";
-                cell.appendChild(piece);
-                }
-            }
-            row.appendChild(cell);
+  constructor(grid) {
+    super([8, 8], ["red", "black"]);
+    this.currentPlayer = 0;
+    this.grid = grid;
+  }
+
+  drawBoard() {
+    const board = document.getElementById("board");
+    board.innerHTML = ""; // clear the board before drawing the new board
+    for (let i = 0; i < 8; i++) {
+      const row = document.createElement("tr");
+      for (let j = 0; j < 8; j++) {
+        const cell = document.createElement("td");
+        if ((i + j) % 2 === 0) {
+          cell.className = "black";
+          const pieceValue = this.grid[i][j];
+          if (pieceValue === 1) {
+            const piece = document.createElement("div");
+            piece.className = "piece red";
+            cell.appendChild(piece);
+          } else if (pieceValue === -1) {
+            const piece = document.createElement("div");
+            piece.className = "piece white";
+            cell.appendChild(piece);
           }
-          board.appendChild(row);
         }
+        row.appendChild(cell);
       }
+      board.appendChild(row);
+    }
+  }
+
       makeMove(from, to) {
         const fromRow = parseInt(from.charAt(0));
         const fromCol = from.charAt(1);
         const toRow = parseInt(to.charAt(0));
         const toCol = to.charAt(1);
-        console.log(`Moving from row ${fromRow}, column ${fromCol} to row ${toRow}, column ${toCol}`);
-    
-        const fromCell = document.querySelector(`#board tr:nth-child(${fromRow}) td:nth-child(${fromCol.charCodeAt(0) - 96})`);
-        const toCell = document.querySelector(`#board tr:nth-child(${toRow}) td:nth-child(${toCol.charCodeAt(0) - 96})`);
-    
-        const piece = fromCell.querySelector(".piece");
-        if (piece) {
-            fromCell.removeChild(piece);
-            toCell.appendChild(piece);
-        }
+        console.log(`Moving from row ${fromRow}, column ${fromCol.charCodeAt(0) - 96} to row ${toRow}, column ${toCol.charCodeAt(0) - 96}`);
+        
+        this.grid[toRow-1][(toCol.charCodeAt(0) - 96) -1] = this.grid[fromRow-1][(fromCol.charCodeAt(0) - 96)-1];
+
+        this.grid[fromRow-1][(fromCol.charCodeAt(0) - 96)-1] = 0;
+
+        console.log(this.grid);
+        this.drawBoard();
     }
     
     
-
       init() {
-        this.draw_board();
+        this.drawBoard();
         const connectButton = document.getElementById("but");
         connectButton.addEventListener("click", () => {
           const fromInput = document.getElementById("from-input");
@@ -58,6 +60,17 @@ export default class Checkers extends GameEngine {
           this.makeMove(from, to);
         });
       }
+      
 }
-const game = new Checkers();
+var grid = [
+  [-1, 0, -1, 0, -1, 0, -1, 0],
+  [0, -1, 0, -1, 0, -1, 0, -1],
+  [-1, 0, -1, 0, -1, 0, -1, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 1, 0, 1, 0, 1, 0, 1],
+  [1, 0, 1, 0, 1, 0, 1, 0],
+  [0, 1, 0, 1, 0, 1, 0, 1],
+];
+const game = new Checkers(grid);
 game.init();
