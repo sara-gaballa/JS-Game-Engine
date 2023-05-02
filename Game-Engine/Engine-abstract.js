82c816dfff76cc97f55c0813fae6fa3995070241
Maria Bassem <maria.bassem2002@gmail.com>
2023-05-02 10:02:54 +0300

    update game engine

--- Game-Engine/Engine-abstract.js
+++ Game-Engine/Engine-abstract.js
@@ -1,9 +1,14 @@
-export class GameEngine {
-    constructor(boardSize, pieces) {
-      this.boardSize = boardSize;
-      this.pieces = pieces;
-      // this.board = Array.from({ length: boardSize[0] }, () => Array.from({ length: boardSize[1] }, () => null));
-    }
+class GameEngine {
+  constructor(boardSize, pieces) {
+    this.boardSize = boardSize;
+    this.pieces = pieces;
+    // this.board = Array.from({ length: boardSize[0] }, () => Array.from({ length: boardSize[1] }, () => null));
+    const readline = require('readline');
+    this.rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+  }
     drawBoard(grid){}
     
     isValid(input){
@@ -22,28 +27,82 @@ export class GameEngine {
     //   }
     // }
     
-    takeInputFromUser(){
-      throw new Error("Method 'takeInputFromUser()' must be implemented in each class.");
+    // takeInputFromUser(){ // 4a 5, 4a ['4a', '5']
+    //   let input = prompt("Enter input");
+    //   // throw new Error("Method 'takeInputFromUser()' must be implemented in each class.");
+    //   if(arguments.length === 2){
+    //     // chess or checkers or sudoko
+
+    //   }if(arguments.length === 1){
+    //     // 8 queens or connect or XO
+    //   }
+    //   return input;
+    // }
+
+    takeInputFromUser() {//4a 5c, a, 3a 4, 3a,
+      return new Promise(() => {
+        this.rl.question('Enter a move: ', (input) => {
+
+          console.log(`You entered, ${input.length}`);
+          console.log('In take input:in the game engine');
+        });
+      });
     }
 
-    play(input, grid, turn){ // two players ==> overloading
-      if(arguments.length === 3){
-        console.log("In play: 2 players of the game engine")
-        this.whichPlayer();
-        this.play(input,grid);
-        this.reverseTurns();
-      }else{ // 1 player ==> overloading
-        console.log("In play: 1 player of the game engine")
-        while(!(this.isValid(input))){
-          console.log("Invalid move.\nPlease enter a valid move.");
-          input = this.takeInputFromUser();
+    controller(){}
+
+    
+
+  //   play(input, grid, turn){ // two players ==> overloading
+  //     if(arguments.length === 3){
+  //       console.log("In play: 2 players of the game engine")
+  //       this.whichPlayer();
+  //       this.play(input,grid);
+  //       this.reverseTurns();
+  //     }else{ // 1 player ==> overloading
+  //       console.log("In play: 1 player of the game engine")
+  //     //   while(!(this.isValid(input))){
+  //     //     console.log("Invalid move.\nPlease enter a valid move.");
+  //     //     input = this.takeInputFromUser();
+  //     //   }
+  //     //   console.log("After input in play")
+  //     //   let newState = this.makeMove(input);
+  //     //   this.drawBoard(newState);
+  //     // }
+  //     while(true){
+  //       let input = this.takeInputFromUser();
+  //       controller();
+  //       this.drawBoard()
+  //     }
+  //   }
+  // }
+
+  play(grid, turn) { // two players ==> overloading
+      if (arguments.length === 2) {
+        console.log('In play: 2 players of the game engine');
+        this.whichPlayer(turn);
+        this.play(grid);
+        this.reverseTurns(turn);
+      } else { // 1 player ==> overloading
+        console.log('In play: 1 player of the game engine');
+        // while (!(this.isValid(input))) {
+        //   console.log('Invalid move.\nPlease enter a valid move.');
+        //   input = this.takeInputFromUser();
+        // }
+        // console.log('After input in play');
+        // let newState = this.makeMove(input);
+        // this.drawBoard(newState);
+        while(true){
+          this.takeInputFromUser().then((input) => {
+            let valid = this.controller(input,grid);
+            if(valid){
+              this.drawBoard(grid);
+            }
+          });
         }
-        console.log("After input in play")
-        let newState = this.makeMove(input);
-        this.drawBoard(newState);
       }
-    }
-
+    
+  }
     whichPlayer(){
       throw new Error("Method 'whichPlayer()' must be implemented in each class.");
     }
@@ -51,3 +110,8 @@ export class GameEngine {
       throw new Error("Method 'reverseTurns()' must be implemented in each class.");
     } 
 }
+
+const game = new GameEngine([8, 8], ["♔", "♚"]);
+
+// call the play method
+game.play();
