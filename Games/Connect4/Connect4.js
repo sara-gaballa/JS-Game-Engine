e298d9ead7d96d7a9c860eeb52a3c37af613d823
NancyHisham <96303049+NancyHisham@users.noreply.github.com>
2023-05-05 07:27:58 +0300

    connect4

--- Games/Connect4/Connect4.js
+++ Games/Connect4/Connect4.js
@@ -1,24 +1,21 @@
 import { GameEngine } from "../../Game-Engine/Engine-abstract.js";
 
-export default class Connect4 extends GameEngine {
-  constructor(grid) {
-    super([6, 7], ["red", "yellow"]);
-    this.currentPlayer = 1;
-    this.grid = grid;
-  }
+export class Connect4 extends GameEngine {
+  // constructor(grid) {
+  //   super([6, 7], ["red", "yellow"]);
+  //   this.currentPlayer = 1;
+  //   this.grid = grid;
+  // }
 
-  whichPlayer(turn){
-    (turn == 1) ? this.currentPlayer = -1 : this.currentPlayer = 1;
-  }
 
-  drawBoard() {
+  drawBoard(grid) {
     const board = document.getElementById("board");
     board.innerHTML = ""; // clear the board before drawing the new board
     for (let i = 0; i < 6; i++) {
       const row = document.createElement("tr");
       for (let j = 0; j < 7; j++) {
         const cell = document.createElement("td");
-        const pieceValue = this.grid[i][j];
+        const pieceValue = grid[i][j];
         if (pieceValue === 1) {
         const piece = document.createElement("div");
         piece.className = "piece red";
@@ -36,54 +33,59 @@ export default class Connect4 extends GameEngine {
     
   }
 
-      makeMove(col) {
-        console.log("col: "+col);
-        if(this.isValid(col)){
+      makeMove(input,grid,turn) {
             let row = -1;
             for(let i = 5;i>=0;i--){
-                if(this.grid[i][col]==0){
+                if(grid[i][input]==0){
                     row = i;
                     break;
                 }
             }
-            this.grid[row][col] = this.currentPlayer;
-            this.whichPlayer(this.currentPlayer);
-            console.log(this.grid);
-            this.drawBoard();
-        } else{
-            alert("invalid move");
-        }
-        
+            grid[row][input] = turn;
+            console.log(grid);
+            return grid;
     }
     
-    isValid(col){
-        if(this.grid[0][col]!=0){
+    isValid(input,grid,turn){
+        if(grid[0][input]!=0){
             //full 
             return false;
         } else {
             return true;
         }
     }
+
+    takeInputFromUser() {
+      return new Promise(resolve => {
+        const input = prompt("Enter the column number (0)");
+        resolve(input);
+      });
+    }
     
-    
+      // init() {
+      //   this.drawBoard();
+      //   const connectButton = document.getElementById("but");
+      //   connectButton.addEventListener("click", () => {
+      //     const toInput = document.getElementById("to-input");
+      //     const col = toInput.value;
+      //     this.makeMove(col);
+      //   });
+      // }
+
       init() {
-        this.drawBoard();
-        const connectButton = document.getElementById("but");
-        connectButton.addEventListener("click", () => {
-          const toInput = document.getElementById("to-input");
-          const col = toInput.value;
-          this.makeMove(col);
-        });
-      }
-      
+        var grid = [
+          [0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0]
+        ];
+        var noOfPlayers = 2;
+        return {
+          grid: grid,
+          noOfPlayers: noOfPlayers,
+        };
+      }   
 }
-var grid = [
-  [0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0]
-];
-const game = new Connect4(grid);
-game.init();
+
--- Initial-Screen/screen.js
+++ Initial-Screen/screen.js
@@ -6,6 +6,7 @@ import {Sudoku} from "../Games/Sudoku/Sudoku.js";
 import {TicTacToe} from "../Games/Tic-Tac-Toe/Tic-Tac-Toe.js";
 import {Chess} from "../Games/Chess/Chess.js";
 import {EightQueens} from "../Games/Eight-Queens/Eight-Queens.js";
+import {Connect4} from "../Games/Connect4/Connect4.js";
 if (typeof window !== "undefined") {
 
     const queryString = window.location.search;
@@ -23,8 +24,10 @@ if (typeof window !== "undefined") {
         // window.location.href = "../Games/" + game + "/" + game +".html";
         console.log(game);
     } else if (game === 'Connect4') {
+        let connect4 = new Connect4();
+        connect4.play();
         console.log(game);
-        window.location.href = "../Games/" + game + "/" + game +".html";
+        // window.location.href = "../Games/" + game + "/" + game +".html";
     } else if (game === 'Sudoku') {
         let sudoku= new Sudoku();
         sudoku.play();
