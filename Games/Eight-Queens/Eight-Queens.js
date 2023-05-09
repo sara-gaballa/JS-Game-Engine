bc2db9666fc69199ef022ed3b10f6625771f2f0a
NancyHisham <96303049+NancyHisham@users.noreply.github.com>
2023-05-09 20:20:36 +0300

    finito

--- Games/Eight-Queens/Eight-Queens.js
+++ Games/Eight-Queens/Eight-Queens.js
@@ -32,8 +32,8 @@ export class EightQueens extends GameEngine {
     ctx.font = "bold 15px Arial";
     ctx.fillStyle = "#000";
   
-    const num = ["0", "1", "2", "3", "4", "5", "6", "7"];
-    for (let i = 0; i < 8; i++) {
+    const num = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
+    for (let i = 0; i < 9; i++) {
       const y = i * CELL_SIZE + 50 + CELL_SIZE / 2 + 12;
       const x1 = 30;
       const x2 = BOARD_SIZE + 50 + 5;
@@ -62,99 +62,126 @@ export class EightQueens extends GameEngine {
   //     resolve(inputArray);
   //   });
   // }
-  parseInput(input){//a 7
-    if(input.length>3) return[NaN,NaN];
-    const column = input.charAt(0).charCodeAt(0) - 97;
-    const row = parseInt(input.charAt(2));
-    return [column,row];
+  parseInput(input){//a 7 //new input => 0a
+    if(input.length>2) return[NaN,NaN];
+    const to = parseInt(input.charAt(0));
+    const queen = input.charAt(1).charCodeAt(0) - 97;
+
+    return [to,queen];
+    // if(input.length>3) return[NaN,NaN];
+    // const column = input.charAt(0).charCodeAt(0) - 97;
+    // const row = parseInt(input.charAt(2));
+    // return [column,row];
   }
-  makeMove(input,grid) {
-      const column = input[0]; // column is now 0
-      const row = input[1];
-      grid[row][column]=1;
-      grid[8][column]=0;
-      return grid;
+    makeMove(input,grid) {
+        // const column = input[0]; // column is now 0
+        // const row = input[1];
+        // grid[row][column]=1;
+        // grid[8][column]=0;
+        // return grid;
+        
+        let [to,queen] = input;
+        if(grid[to][queen]==1){
+          grid[8][queen]=1;
+          grid[to][queen]=0;
+        } else{
+          grid[8][queen]=0;
+          grid[to][queen]=1;
+        }
+        return grid;
+        
+        
+    }
+
+    isValid(input,grid,turn){
+      let [to,queen] = input;
+      console.log(`${to},${queen}`)
+      // const column = input[0]; // column is now 0
+      // const row = input[1];
+      if(isNaN(to)||isNaN(queen)){
+        console.log("1")
+        alert("invaild input");
+        return false;
+      }
+      // if (!Number.isInteger(toInt)) {
+      //   alert("invaild input");
+      //   return false;
+      // }
+      if(to < 0 || to > 7 || queen < 0 || queen > 7){
+        console.log("2")
+        alert("invaild input");
+        return false;
+      }
+      if(grid[to][queen]==1){
+        return true;
+      }
+  
       
-  }
-
-  isValid(input,grid,turn){//What The???
-    const column = input[0]; // column is now 0
-    const row = input[1];
-    const toInt = parseInt(row, 10);
-    if(isNaN(row)||isNaN(column)){
-      alert("invaild input");
-      return false;
-    }
-    if (!Number.isInteger(toInt)) {
-      alert("invaild input");
-      return false;
-    }
-    console.log("row= "+row+" col = "+column);
-    if(column < 0 || column > 7 || row < 0 || row > 7){
-      alert("invaild input");
-      return false;
-    }
-    else {
-      for(let i = 0 ;i < 8;i++ ){
-        if(grid[row][i]==1){
-          alert("invaild place");
-          return false;
+      else {
+        for(let i = 0 ;i < 8;i++ ){
+          if(grid[to][i]==1){
+            alert("invaild place");
+            return false;
+          }
+        }
+        for(let j = 0 ;j < 8 ;j++){
+          if(grid[j][queen]==1){
+            alert("invaild place");
+            return false;
+          }
+        }
+        let i = to;
+        for(let j = queen ;j >= 0 && i >=0; j-- ){
+          if(grid[i][j]==1){
+            alert("invaild place");
+            return false;
+          }else i--;
+        }
+        let k = to;
+        for(let j = queen ;j < 8 && k != 8; j++ ){
+          if(grid[k][j]==1){
+            alert("invaild place");
+            return false;
+          }else k++;
+        }
+        let l = to;
+        for(let j = queen ;j >= 0 && l != 8; j-- ){
+          if(grid[l][j]==1){
+            alert("invaild place");
+            return false;
+          }else l++;
+        }
+        let p = to;
+        for(let j = queen ;j < 8 && p >= 0; j++ ){
+          if(grid[p][j]==1){
+            alert("invaild place");
+            return false;
+          }else p--;
         }
       }
-      for(let j = 0 ;j < 8 ;j++){
-        if(grid[j][column]==1){
-          alert("invaild place");
-          return false;
-        }
-      }
-      let i = row;
-      for(let j = column ;j >= 0 && i >=0; j-- ){
-        if(grid[i][j]==1){
-          alert("invaild place");
-          return false;
-        }else i--;
-      }
-      let k = row;
-      for(let j = column ;j < 8 && k != 8; j++ ){
-        if(grid[k][j]==1){
-          alert("invaild place");
-          return false;
-        }else k++;
-      }
-      let l = row;
-      for(let j = column ;j >= 0 && l != 8; j-- ){
-        if(grid[l][j]==1){
-          alert("invaild place");
-          return false;
-        }else l++;
-      }
-      let p = row;
-      for(let j = column ;j < 8 && p >= 0; j++ ){
-        if(grid[p][j]==1){
-          alert("invaild place");
-          return false;
-        }else p--;
-      }
-    }
-    return true;
+     return true;
   }
 
-  init() {
-    var grid = [
-      [0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0],
-      [1, 1, 1, 1, 1, 1, 1, 1]
-    ];
-    var noOfPlayers = 1;
-    return {
-      grid: grid,
-      noOfPlayers: noOfPlayers,
-    };
-  }
+
+
+ 
+      init() {
+        var grid = [
+          [0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0],
+          [1, 1, 1, 1, 1, 1, 1, 1]
+        ];
+        var noOfPlayers = 1;
+        return {
+          grid: grid,
+          noOfPlayers: noOfPlayers,
+        };
+      } 
+      
 }
