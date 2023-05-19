4fc9c0ba83b27f0c8fe19ef430bb093d16e8968c
emansamir2 <51717395+emansamir2@users.noreply.github.com>
2023-05-19 23:53:56 +0300

    sudokuprolog

--- Games/Sudoku/Sudoku.js
+++ Games/Sudoku/Sudoku.js
@@ -1,50 +1,7 @@
 import { GameEngine } from "../../Game-Engine/Engine-abstract.js";
+import{SudokuGenerator}from"./SudokoGenerator.js"
 
 export class Sudoku extends GameEngine {
-
-	// Sudoku Generator
-	fillValues(grid) {
-		// Fill the diagonal of SRN x SRN matrices
-		grid=this.fillDiagonal(grid);
-
-		// Fill remaining blocks
-		grid=this.fillRemaining(grid,0, 3);
-
-		// Remove Randomly K digits to make game
-		grid=this.removeKDigits(grid);
-    return grid;
-	}
-
-	// Fill the diagonal SRN number of SRN x SRN matrices
-	fillDiagonal(grid) {
-		for (let i = 0; i < 9; i += 3) {
-			// for diagonal box, start coordinates->i==j
-			grid= this.fillBox(grid,i, i);
-		}
-    return grid;
-	}
-
-	// Fill a 3 x 3 matrix.
-	fillBox(grid,row, col) {
-		let num = 0;
-		for (let i = 0; i < 3; i++) {
-			for (let j = 0; j < 3; j++) {
-				while (true) {
-					num = this.randomGenerator(9);
-					if (this.unUsedInBox(grid,row, col, num)) {
-						break;
-					}
-				}
-				grid[row + i][col + j] = num;
-			}
-		}
-    return grid;
-	}
-
-	// Random generator
-	randomGenerator(num) {
-		return Math.floor(Math.random() * num + 1);
-	}
 
 	// Check if safe to put in cell
 	checkIfSafe(grid,i, j, num) {
@@ -86,48 +43,7 @@ export class Sudoku extends GameEngine {
 		}
 		return true;
 	}
-	// A recursive function to fill remaining
-	// matrix
-	fillRemaining(grid) {
-    for(let i=0;i<9;i++){
-      for(let j=0;j<9;j++){
-        if (grid[i][j] === 0) {
-          for (let num = 1; num <= 9; num++) {
-            if (this.checkIfSafe(grid,i, j, num)) {
-              grid[i][j] = num;
-              // grid[i][j] = 0;
-            }
-          }
-        }
-      }
-    }
-    return grid;
-	}
-
-	// Print sudoku
-	printSudoku(grid) {
-		for (let i = 0; i < 9; i++) {
-				console.log(grid[i].join(" "))
-		}
-	}
-
-	// Remove the K no. of digits to
-	// complete game
-	removeKDigits(grid) {
-		let count = 45;
-
-		while (count !== 0) {
-			// extract coordinates i and j
-			let i = Math.floor(Math.random() * 9);
-			let j = Math.floor(Math.random() * 9);
-			if (grid[i][j] !== 0) {
-				count--;
-				grid[i][j] = 0;
-			}
-		}
-
-		return grid;
-	}
+	
 
   drawBoard(grid) {
     const board = document.getElementById("board");
@@ -162,19 +78,114 @@ export class Sudoku extends GameEngine {
   async solve(grid){
 
     const session = pl.create();
-    const knowledge_base = ``;
+    const knowledge_base = `
+    :- use_module(library(apply)).
+    :- use_module(library(clpfd)).
+    :- use_module(library(clpb)).
+    :- use_module(library(lists)).
+    :- use_module(library(ISO)).
+    :- use_module(library(clpfd)).
+
+    
+    convert_array([], []).
+    convert_array([Row|Rest], [NewRow|NewRest]) :-
+        write('in convert first'),
+        convert_row(Row, NewRow),  
+        convert_array(Rest, NewRest).  
+
+
+    convert_row([], []).
+    convert_row([0|Rest], [_|NewRest]) :-
+         write('in convert row3'),
+        convert_row(Rest, NewRest).  
+
+
+    convert_row([Z|Rest], [Z|NewRest]) :-
+        write('in convert row1'),
+        Z \\= 0,
+        write('in convert row2'),
+        convert_row(Rest, NewRest).  
+
+
+    solve(Array,X):-
+      write('in solve first'+Array),
+      convert_array(Array, X),
+      write('in solve after convert'+X),
+      sudoku(X).
+
+
+
+      
+    sudoku(Puzzle) :-
+        write('in sodoku first'+Puzzle),
+        mflatten(Puzzle, Tmp),
+        write('in sodoku after flatten'+Tmp),
+        manualll(Tmp, 1, 9),
+        write('in sodoku after ins'+Puzzle),
+        Rows = Puzzle,
+        transpose(Rows, Columns),
+        blocks(Rows, Blocks),
+        maplist(all_distinct, Rows),
+        maplist(all_distinct, Columns),
+        maplist(all_distinct, Blocks),
+        maplist(label, Rows).
+      
+
+    mflatten(List, FlatList) :-
+      mflatten(List, [], FlatList0),
+      !,
+      FlatList = FlatList0.
+  
+    mflatten(Var, Tl, [Var|Tl]) :-
+      var(Var),
+      !.
+    mflatten([], Tl, Tl) :- !.
+    
+    mflatten([Hd|Tl], Tail, List) :-
+      !,
+      mflatten(Hd, FlatHeadTail, List),
+      mflatten(Tl, Tail, FlatHeadTail).
+      mflatten(NonList, Tl, [NonList|Tl]).  
+      
+
+    manualll([], _, _).
+    manualll([X|Xs], Min, Max) :-
+      write('in inssssss'),
+      X #>= Min,
+      X #=< Max,
+      manualll(Xs, Min, Max).
+    
+
+    blocks([A,B,C,D,E,F,G,H,I], Blocks) :-
+      blocks(A,B,C,Block1), blocks(D,E,F,Block2), blocks(G,H,I,Block3),
+      append([Block1, Block2, Block3], Blocks).
+
+    blocks([], [], [], []).
+    blocks([A,B,C|Bs1],[D,E,F|Bs2],[G,H,I|Bs3], [Block|Blocks]) :-
+      Block = [A,B,C,D,E,F,G,H,I],
+      blocks(Bs1, Bs2, Bs3, Blocks).
+
+
+  
+    `;
 
     let found=false;
 
     session.consult(knowledge_base);
-    let GridP;
+    let T=grid;
+    console.log(JSON.stringify(grid));
     for(let i=0;i<9;i++){
       for(let j=0;j<9;j++){
-        GridP[i][j]=grid[i][j]
-        if(grid[i][j]<0)GridP[i][j]*=(-1);
+        if(grid[i][j]<0) {
+          T[i][j]=grid[i][j]*(-1);
+        }
+        else {
+          T[i][j]=grid[i][j];
+        }
       }
     }
-    const query = `convert(${JSON.stringify(GridP)}, X).`;
+    console.log(JSON.stringify(T));
+    const query = `solve(${JSON.stringify(T)},X).`;
 
     session.query(query,{
         success: function(goal){
@@ -228,10 +239,18 @@ export class Sudoku extends GameEngine {
   }
 
 
-  makeMove(input,grid) {
+  async makeMove(input,grid) {
     let [i,j,num] = input;
-    console.log(`to row ${i}, column ${j}`);
-    grid[i][j]=num;
+    if(i==0&& j==-49&&num==0){
+      //solve
+      console.log("solve");
+      grid=await this.solve(grid); 
+
+    }else{
+      console.log(`to row ${i}, column ${j}`);
+      grid[i][j]=num;
+    }
+    
     return grid;
   }
     
@@ -241,7 +260,12 @@ export class Sudoku extends GameEngine {
       alert("Invalid input!!");
       return false;
     }
-    if(i>=0 && i<=8 && j>=0 && j<=8 && num>=0 && num<=9 ){
+    if(i==0&& j==-49&&num==0){
+      //solve
+      console.log("solve");
+      return true;
+    }
+    else if(i>=0 && i<=8 && j>=0 && j<=8 && num>=0 && num<=9 ){
       if(num >0 ){//add
         if( grid[i][j]>=0 && this.checkIfSafe(grid,i, j, num)){
           return true;
@@ -279,15 +303,17 @@ export class Sudoku extends GameEngine {
       [0, 0, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0],
     ];
-    
-    grid=this.fillValues(grid);
+    let generator=new SudokuGenerator(9,45);
+    generator.fillValues();
+    grid=generator.mat;
+   
     for (let i = 0; i < 9; i++) {
       for (let j = 0; j < 9; j++) {
         grid[i][j] *= -1
       }
     }
-    this.printSudoku(grid);
     var noOfPlayers = 1;
+    console.log(grid);
     return {
       grid: grid,
       noOfPlayers: noOfPlayers,
