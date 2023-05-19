aef41a094ffab5fe50970e878985de618511f36e
emansamir2 <51717395+emansamir2@users.noreply.github.com>
2023-05-19 05:53:52 +0300

    ....

--- Games/Eight-Queens/Eight-Queens.js
+++ Games/Eight-Queens/Eight-Queens.js
@@ -66,16 +66,6 @@ export class EightQueens extends GameEngine {
     :- use_module(library(lists)).
     :- use_module(library(tipc/tipc_linda)).
     
-  
-    transposeBoard([], []).
-    transposeBoard([[]|_], []).
-    transposeBoard(Matrix, [FirstCol|TransposedRest]) :-
-      extractFirstColumn(Matrix, FirstCol, RestMatrix),
-      transposeBoard(RestMatrix, TransposedRest).
-    
-    extractFirstColumn([], [], []).
-    extractFirstColumn([[X|Xs]|Matrix], [X|FirstCol], [Xs|RestMatrix]) :-
-      extractFirstColumn(Matrix, FirstCol, RestMatrix).
     
     convertColumns([], []).
     convertColumns([Column|Rest], [ConvertedCol|ConvertedRest]) :-
@@ -96,37 +86,36 @@ export class EightQueens extends GameEngine {
       convertColumnHelper(Rest, NewRow, ConvertedCol).
 
 
-      queens(N, Queens) :-
+    queens(N, Queens) :-
       length(Queens, N),
-    board(Queens, Board, 0, N, _, _),
-    queens(Board, 0, Queens).
+      board(Queens, Board, 0, N, _, _),
+      queens(Board, 0, Queens).
   
-  board([], [], N, N, _, _).
-  board([_|Queens], [Col-Vars|Board], Col0, N, [_|VR], VC) :-
-    Col is Col0+1,
-    functor(Vars, f, N),
-    constraints(N, Vars, VR, VC),
-    board(Queens, Board, Col, N, VR, [_|VC]).
-  
-  constraints(0, _, _, _) :- !.
-  constraints(N, Row, [R|Rs], [C|Cs]) :-
-    arg(N, Row, R-C),
-    M is N-1,
-    constraints(M, Row, Rs, Cs).
-  
-  queens([], _, []).
-  queens([C|Cs], Row0, [Col|Solution]) :-
-    Row is Row0+1,
-    select(Col-Vars, [C|Cs], Board),
-    arg(Row, Vars, Row-Row),
-    queens(Board, Row, Solution).
+    board([], [], N, N, _, _).
+    board([_|Queens], [Col-Vars|Board], Col0, N, [_|VR], VC) :-
+      Col is Col0+1,
+      functor(Vars, f, N),
+      constraints(N, Vars, VR, VC),
+      board(Queens, Board, Col, N, VR, [_|VC]).
+    
+    constraints(0, _, _, _) :- !.
+    constraints(N, Row, [R|Rs], [C|Cs]) :-
+      arg(N, Row, R-C),
+      M is N-1,
+      constraints(M, Row, Rs, Cs).
+    
+    queens([], _, []).
+    queens([C|Cs], Row0, [Col|Solution]) :-
+      Row is Row0+1,
+      select(Col-Vars, [C|Cs], Board),
+      arg(Row, Vars, Row-Row),
+      queens(Board, Row, Solution).
 
 
-    convert(Board, Converted) :-
-    transposeBoard(Board, Transposed),
-    convertColumns(Transposed, Converted),
-    !, % Cut operator - prevents backtracking
-    queens(8,Converted).
+      convert(Board, Converted) :-
+      convertColumns(Board, Converted),
+      !, % Cut operator - prevents backtracking
+      queens(8,Converted).
   
 
 `;
@@ -171,7 +160,7 @@ if(found){
     }
   }
   for(let i=0;i<8;i++){
-    grid[GridP[i]-1][i]=1;
+    grid[i][GridP[i]-1]=1;
   }
 }else{
   alert("No solution");
